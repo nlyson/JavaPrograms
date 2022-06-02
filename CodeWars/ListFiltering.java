907ab1960b33aae536eaa0fcb63620b9829e78e8
nlyson <72270566+nlyson@users.noreply.github.com>
2022-06-02 12:15:57 -0500

    Update ListFiltering.java

--- CodeWars/ListFiltering.java
+++ CodeWars/ListFiltering.java
@@ -15,7 +15,7 @@ public class ListFiltering {
   
    public static List<Object> filterList(final List<Object> list) 
    {
-      // Return the List with the Strings filtered out
+      // Create a new list - use instanceof to only add non-String objects to the new list
       List<Object> filteredList = new ArrayList<Object>();
       for(Object o : list)
       {
